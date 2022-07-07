02015f7d096603f36df22139434133f82f26bd15
truongzodinet <truong.tran@zodinet.tech>
2022-07-07 15:09:04 +0700

    style(responsive): footer layout

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,5 @@
-const mobile_toggle = document.querySelector('#navigation_toggle');
+const navigation_toggle_close = document.querySelector('#navigation_toggle_close');
+const navigation_toggle_open = document.querySelector('#navigation_toggle_open');
 const mobile_list = document.querySelector('#mobile_navigation_list');
 
 let isOpenToggle = false;
@@ -6,13 +7,20 @@ let isOpenToggle = false;
 const openToogle = () => {
     isOpenToggle = true;
     mobile_list.style.display = 'flex';
+    navigation_toggle_close.style.display = 'block';
+    navigation_toggle_open.style.display = 'none';
 }
 const closeToogle = () => {
     isOpenToggle = false;
     mobile_list.style.display = 'none';
+    navigation_toggle_close.style.display = 'none';
+    navigation_toggle_open.style.display = 'block';
 }
 
-mobile_toggle.addEventListener('click', () => {
+navigation_toggle_open.addEventListener('click', () => {
+    isOpenToggle ? closeToogle() : openToogle();
+})
+navigation_toggle_close.addEventListener('click', () => {
     isOpenToggle ? closeToogle() : openToogle();
 })
 
